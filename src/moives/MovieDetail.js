c0f25357dfe0b8a5c57f6c58655780a76c92fb70
zubaidr <web.expert1221@gmail.com>
2022-05-30 20:02:25 +0500

    add a grid and import compenents

--- src/moives/MovieDetail.js
+++ src/moives/MovieDetail.js
@@ -5,7 +5,9 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { useParams } from 'react-router-dom';
-
+import {SimilarMovies} from './SimilarMovies';
+import { MoviesReview } from './MoviesReview';
+import Rating from '@mui/material/Rating';
 export function MovieDetail() {
     const [movies, setMovies] = useState([])
     const  {id } = useParams()
@@ -14,38 +16,43 @@ export function MovieDetail() {
         return response.json()
       }).then(value => {
         setMovies(value)
-        console.log(value) 
+        // console.log(value) 
       })
     },[id])
+
     console.log('id',id)
     return (
-        <Box sx={{ display: 'flex',justifyContent:'center',marginTop:4,}}>
-
+        <Box sx={{marginTop:4,}} >
             <Card sx={{ display: 'flex', pl:10,pt:6,pb:6}}>
                 <CardMedia
                     component="img"
-                    sx={{ width: 400 }}
-                    image="https://i.tribune.com.pk/media/images/2000433-endgameposter-1561532245/2000433-endgameposter-1561532245.jpg"
+                    sx={{ width: 350, height:'350px'}}
+                    image="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/7Bttz4hEspKlpU0Me57dkHNR3nf.jpg"
                     alt="nature"
                 />
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                     <CardContent sx={{ flex: '1 0 auto' }}>
                         <Typography component="div" variant="h5">  
-                         {movies.title} 
+                         {movies.title} ({movies.release_date})
                         </Typography>
                         <Typography variant="subtitle1" color="text.secondary" 
                         component="div">
                             <h3>Overview</h3>  
                         {movies.overview}
                         </Typography>
-                    </CardContent>
-                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                        <Typography variant="subtitle1" color="text.secondary" 
+                        component="div">
+                        <Rating name="read-only" value={movies.vote_average/2} readOnly /> 
+                        </Typography>
                        
+                    </CardContent>
+                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>    
                     </Box>
                 </Box>
-
+              
             </Card>
-
+            <SimilarMovies/>
+            <MoviesReview/>
         </Box>
     )
 }

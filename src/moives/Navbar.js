61ce111a91f915c2dfc2a3012739b8886e16e8ab
zubaidr <web.expert1221@gmail.com>
2022-05-30 20:05:29 +0500

    minner changes

--- src/moives/Navbar.js
+++ src/moives/Navbar.js
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { ImageListItem, Box, AppBar, Toolbar, ListItem, ListItemButton, Button, TextField } from '@mui/material';
+import { Box, AppBar, Toolbar, ListItem, ListItemButton, Button, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import './Movies.css';
-import { SettingsInputHdmiTwoTone } from '@mui/icons-material';
-
-
+import { useNavigate } from 'react-router-dom';
 export default function Navbar() {
   const [search, setSearch] = useState(true)
   const [searchData, setSearchData] = useState([])
-
-
+  const navigation = useNavigate()
   const [input, setInput] = useState('a')
-
   const serachBox = () => {
     setSearch(!search)
-
   }
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=f6bb554383b810d88fb570d3cf4c92a6&language=en-US&query=${input}&page=2&include_adult=false`).then(response => {
@@ -25,16 +20,11 @@ export default function Navbar() {
     }).then(value => {
       setSearchData(value.results)
     })
-
   }, [input])
-
-  const searchItem=(data)=>{
-    console.log('i',data)
-
+  const searchItem = (data) => {
+    console.log('i', data)
+    navigation(`detail/${data.id}`)
   }
-  console.log("ff", searchData)
-  console.log("dd", input)
-
   return (
     <Box className='container' >
       <AppBar>
@@ -62,21 +52,20 @@ export default function Navbar() {
               <Button color="inherit">Login</Button>
               <Button color="inherit">JoinTMDB</Button>
               <Autocomplete
-                 disablePortal
+                disablePortal
                 id="combo-box-demo"
                 sx={{ width: 300 }}
                 disableClearable
                 options={searchData}
-                getOptionLabel={(data)=>data.title}
-                onChange={(value)=>searchItem(value)}
+                getOptionLabel={(data) => data.title}
+                onChange={(event, value) => searchItem(value)}
                 renderInput={(params) => (
                   <TextField className="search"
                     {...params}
                     label="Search input"
                     type='search'
                     value={input}
-
-                    onChange={(e)=>setInput(e.target.value)}
+                    onChange={(e) => setInput(e.target.value)}
                   />
                 )}
               />
@@ -84,11 +73,7 @@ export default function Navbar() {
             </Box>
           </Box>
         </Toolbar>
-        {/* {search ? '' : */}
-
-        {/* }  */}
       </AppBar>
-
     </Box>
   );
 };
